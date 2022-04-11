1bf8a6e6eeb3c377f1ffb5f394766ae676c7e2ab
Dipankar <dipankar15-11137@diu.edu.bd>
2022-04-12 00:51:39 +0600

    sipping add

---
+++ src/components/Shipment/Shipment.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+
+const Shipment = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [address, setAddress] = useState('');
+    const [Phone, setPhone] = useState('');
+    const [error, setError] = useState('');
+    // const navigate = useNavigate();
+
+    const handleNameBlur = event => {
+        setName(event.target.value);
+    }
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handleAddressBlur = event => {
+        setAddress(event.target.value);
+    }
+
+    const handlePhoneBlur = event => {
+        setPhone(event.target.value);
+    }
+
+    // if (user) {
+    //     navigate('/shop');
+    // }
+
+    const handleCreateUser = event => {
+        event.preventDefault();
+
+    }
+
+    return (
+        <div className='from-container'>
+            <div>
+                <h2 className='from-title'>Sign Up</h2>
+                <form onSubmit={handleCreateUser}>
+                    <div className="input-group">
+                        <label htmlFor="name">Email</label>
+                        <input onBlur={handleEmailBlur} type="text" name="name" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Address</label>
+                        <input onBlur={handleAddressBlur} type="text" name="address" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Phone Number</label>
+                        <input onBlur={handlePhoneBlur} type="text" name="phone" id="" />
+                    </div>
+                    <p style={{ color: 'red' }}>{error}</p>
+                    <input className='from-submit' type="submit" value="Add sepping" required />
+                </form>
+                <p>
+
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Shipment;
